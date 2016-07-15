c1ef7407a8e02c88de241b53237c0240bf98066d
rishigoel <goelrishi12@gmail.com>
2016-07-14 21:10:52 -0400

    have div on page.  Just need to load website within the div

---
+++ content.js
@@ -0,0 +1,18 @@
+document.documentElement.style.height = '100%';
+document.body.style.height = '100%';
+document.documentElement.style.width = '105%';
+document.body.style.width = '105%';
+
+var div = document.createElement('div');
+document.body.appendChild(div);
+div.id = 'webview';
+
+div.style.position = 'fixed';
+div.style.top = '0%';
+div.style.right = '0%';
+div.style.width = '25%';   
+div.style.height = '100%';
+div.style.backgroundColor = 'blue';
+
+
+
