9a37c8eb85134b7dd97a8e8038c8223984b92522
rishigoel <goelrishi12@gmail.com>
2016-07-14 22:40:14 -0400

    working version with bing news loaded.  Needs styling/code clean up

--- content.js
+++ content.js
@@ -3,16 +3,14 @@ document.body.style.height = '100%';
 document.documentElement.style.width = '105%';
 document.body.style.width = '105%';
 
-var div = document.createElement('div');
-document.body.appendChild(div);
-div.id = 'webview';
+var iframe = document.createElement('IFRAME');
+document.body.appendChild(iframe);
+iframe.id = 'webview';
 
-div.style.position = 'fixed';
-div.style.top = '0%';
-div.style.right = '0%';
-div.style.width = '25%';   
-div.style.height = '100%';
-div.style.backgroundColor = 'blue';
-
-
+iframe.style.position = 'fixed';
+iframe.style.top = '5%';
+iframe.style.right = '0%';
+iframe.style.width = '25%';   
+iframe.style.height = '100%';
+iframe.src = chrome.runtime.getURL("index.html");
 
