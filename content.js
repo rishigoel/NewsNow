39261197b67c1f9a8e7ab88a75d1ec66142d27b8
rishigoel <goelrishi12@gmail.com>
2016-07-17 21:39:17 -0400

    changed icon/fixed some styling

--- content.js
+++ content.js
@@ -1,16 +1,17 @@
 document.documentElement.style.height = '100%';
 document.body.style.height = '100%';
-document.documentElement.style.width = '105%';
-document.body.style.width = '105%';
+document.documentElement.style.width = '100%';
+document.body.style.width = '100%';
 
 var iframe = document.createElement('IFRAME');
 document.body.appendChild(iframe);
-iframe.id = 'webview';
 
 iframe.style.position = 'fixed';
-iframe.style.top = '5%';
+iframe.style.top = '7%';
 iframe.style.right = '0%';
-iframe.style.width = '25%';   
-iframe.style.height = '100%';
+iframe.style.width = '20%';   
+iframe.style.height = '25%';	
+iframe.style.border = '0px';
 iframe.src = chrome.runtime.getURL("index.html");
+
 
